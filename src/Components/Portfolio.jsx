6e6403f8fe219345549fa386441ffcf2e92495d8
rahulgajraa <152686793+rahulgajraa@users.noreply.github.com>
2024-07-02 19:49:38 +0530

    first commit

---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,80 @@
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+
+import React from "react";
+
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "../images/design-desk.jpeg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "portfolio website",
+    description:
+      "This website tells about my qualifications and achievements. It has given me lots of learning and success",
+    url: "https://gajracodelab.netlify.app/",
+  },
+  {
+    title: "food delivery app",
+    description:
+      "By using this application, our users can order food online. They simply visit our site and order food as per their choice",
+    url: "https://github.com/rahulgajra/Food-Delivery-App",
+  },
+  {
+    title: "weathe app",
+    description:
+      "our this app is give us weather update at real time.",
+    url: "https://github.com/rahulgajra/Weather-App",
+  }
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+              </a>
+              <p className="small">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
